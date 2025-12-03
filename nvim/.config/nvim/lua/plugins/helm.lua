8bc65e7d8e9ea68bc63115e8e615319adf6c276a
IMidULti <961489+IMidULti@users.noreply.github.com>
2025-12-03 00:11:29 -0800

    added helm support

--- nvim/.config/nvim/lua/plugins/helm.lua
+++ nvim/.config/nvim/lua/plugins/helm.lua
@@ -2,4 +2,12 @@
 return {
   "towolf/vim-helm",
   ft = { "helm", "gotmpl" },
+  config = function()
+    vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+      pattern = { "*argo*/*.yaml", "*argo*/*.yml" },
+      callback = function(args)
+        vim.bo[args.buf].filetype = "helm"
+      end,
+    })
+  end,
 }
