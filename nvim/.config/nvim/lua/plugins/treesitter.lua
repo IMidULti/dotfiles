42e694875f190c89262ed15f838293430515e42e
IMidULti <961489+IMidULti@users.noreply.github.com>
2026-06-27 11:42:45 -0700

    Testing 0.12 config

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -3,8 +3,21 @@
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
   opts = {
-    ensure_installed = {
+    highlight = { enable = true },
+    indent = { enable = true },
+  },
+  init = function()
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function()
+        -- Enable treesitter highlighting and disable regex syntax
+        pcall(vim.treesitter.start)
+        -- Enable treesitter-based indentation
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
+    local ensureInstalled = {
       "lua",
       "vim",
       "go",
@@ -17,13 +30,21 @@ return {
       "gotmpl",
       "hcl",
       "terraform",
-    },
-    highlight = { enable = true },
-    indent = { enable = true },
-  },
+    }
+    local alreadyInstalled = require("nvim-treesitter.info").installed_parsers()
+    local parsersToInstall = vim.iter(ensureInstalled)
+      :filter(function(parser)
+        return not vim.tbl_contains(alreadyInstalled, parser)
+      end)
+      :totable()
+    local install_module = require("nvim-treesitter.install")
+    if install_module and install_module.ensure_installed then
+      install_module.ensure_installed(parsersToInstall)
+    end
+  end,
   config = function(_, opts)
     -- Map gohtml filetype to gotmpl parser
     vim.treesitter.language.register("gotmpl", "gohtml")
-    require("nvim-treesitter.configs").setup(opts)
+    require("nvim-treesitter").setup(opts)
   end,
 }
